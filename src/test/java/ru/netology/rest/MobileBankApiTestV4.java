cf58efa25fc4aefe8f2601e569429e53315f743a
irina <obesnyuk@gmail.com>
2023-12-10 19:36:38 +0300

    first test commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV4 {
     @Test
@@ -19,6 +20,8 @@ class MobileBankApiTestV4 {
       .then()
           .statusCode(200)
           .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+              .body("[0].currency", equalTo("RUB"))
+              .body("[1].currency", equalTo("USD"))
       ;
     }
 }
